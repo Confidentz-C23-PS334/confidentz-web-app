a17b03a61bf5f06354151c6c08d9bb8287a127b2
ahmaddynugroho <ahmaddwi700@gmail.com>
2023-06-07 00:36:44 +0700

    feat: check-carries page

--- src/views/CheckCarriesView.vue
+++ src/views/CheckCarriesView.vue
@@ -1,3 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const file = ref(null)
+const confidence = ref(0)
+const setFile = (e) => {
+  file.value = e.target.files[0]
+}
+const upload = () => {
+  const formData = new FormData()
+  formData.append('file', file.value)
+  const options = {
+    method: 'POST',
+    body: formData,
+  }
+  fetch('/detect', options)
+    .then((res) => res.json())
+    .then(data => confidence.value = data.carries_confidence)
+}
+</script>
+
 <template>
-  <h1>this is check carries</h1>
+  <input type="file" name="file" id="file" @change="setFile" />
+  <button @click="upload">submit</button>
+  <p v-if="confidence">confidence: {{ confidence }}</p>
 </template>
