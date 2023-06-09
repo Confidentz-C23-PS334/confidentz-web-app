17d49d258be1b1fe5a00f21ac6fefaaf5d442a3a
ahmaddynugroho <ahmaddwi700@gmail.com>
2023-06-10 00:59:03 +0700

    refactor: use firebase modular API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,47 @@
+<script setup lang="ts">
+import { reactive, onMounted } from 'vue'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '@/firebase'
+
+const collectionName = 'artikel'
+
+interface Article {
+  id: string
+  title: string
+  author: string
+  date: string
+  body: string
+  website: string
+}
+const articles: Article[] = reactive([])
+
+const getCollection = async () => {
+  const querySnapshot = await getDocs(collection(db, collectionName))
+  querySnapshot.forEach((doc) => {
+    const data = doc.data()
+
+    const article: Article = {
+      id: doc.id,
+      title: data.title,
+      author: data.author,
+      date: data.date,
+      body: data.body,
+      website: data.website,
+    }
+
+    // Menambahkan artikel ke dalam array
+    articles.push(article)
+  })
+}
+
+onMounted(() => getCollection())
+</script>
+
 <template>
   <div>
     <h1>Artikel Seputar Kesehatan Gigi</h1>
-    <div class="card-container">
+    <h2 v-if="!articles[0]">fetching articles...</h2>
+    <div v-else class="card-container">
       <div v-for="article in articles" :key="article.id" class="card">
         <h2>{{ article.title }}</h2>
         <p>{{ article.author }}</p>
@@ -13,52 +53,12 @@
   </div>
 </template>
 
-<script>
-import { db } from "@/firebase"; // impor inisialisasi Firebase dan referensi koleksi Firestore
-
-export default {
-  data() {
-    return {
-      articles: [], // Simpan data artikel
-    };
-  },
-  mounted() {
-    // Mengambil data dari koleksi "artikel" di Firestore
-    db.collection("artikel")
-      .get()
-      .then((querySnapshot) => {
-        // Mengiterasi setiap dokumen
-        querySnapshot.forEach((doc) => {
-          // Mendapatkan data dari dokumen
-          const data = doc.data();
-
-          // Membuat objek artikel dengan properti yang sesuai
-          const article = {
-            id: doc.id,
-            title: data.title,
-            author: data.author,
-            date: data.date,
-            body: data.body,
-            website: data.website
-          };
-
-          // Menambahkan artikel ke dalam array
-          this.articles.push(article);
-        });
-      })
-      .catch((error) => {
-        console.error("Error getting documents:", error);
-      });
-  },
-};
-</script>
-
 <style>
 .card-container {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  gap: 16px; 
+  gap: 16px;
 }
 
 .card {
